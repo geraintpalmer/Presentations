43f2ea36b918ea09fcc2fe7361a3863590af135d
Geraint Palmer <palmer.geraint@googlemail.com>
2016-03-19 10:26:18 +0000

    IMA add vince suggestions

--- IMAconference/ciwexample.py
+++ IMAconference/ciwexample.py
@@ -1,17 +1,24 @@
->>> import ciw
->>> params = {
-...   'Number_of_nodes': 2,
-...   'Arrival_distributions':
-...      [['Exponential', 6.0],
-...       ['Uniform', 0.2, 0.4]],
-...   'Service_distributions':
-...      [['Lognormal', 0.4, 0.3],
-...       ['Deterministic', 0.25]],
-...   'Transition_matrices':
-...      [[0.5, 0.1],
-...       [0.0, 0.4]],
-...   'Number_of_servers': [3, 1],
-...   'Simulation_time': 2000}
->>> Q = ciw.Simulation(params)
->>> Q.simulate_until_max_time()
->>> recs = Q.get_all_records()
+params = {
+  'Number_of_nodes': 2,
+  'Number_of_classes': 2,
+  'Number_of_servers': [3, 1],
+  'Simulation_time': 2000,
+
+  'Arrival_distributions':{
+    'Class 0': [['Exponential', 6.0],
+                ['Uniform', 0.2, 0.3]],
+    'Class 1': [['Lognormal', 0.4, 0.3],
+                ['Deterministic', 0.25]]},
+
+  'Service_distributions':{
+    'Class 0': [['Exponential', 7.0],
+                ['Deterministic', 0.3]],
+    'Class 1': [['Exponential', 7.0],
+                ['Exponential', 4.5]]},
+                
+  'Transition_matrices':{
+    'Class 0': [[0.0, 0.5],
+                [0.0, 0.8]],
+    'Class 1': [[0.2, 0.0],
+                [0.2, 0.4]]}
+}
